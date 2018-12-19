07d81ceaf98d4221be4ca86e1c9a8fa915cfa314
Joni Agüero <aguerojonathanivan@gmail.com>
2018-12-19 17:27:56 -0300

    Forms & Routes

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
-import Layout from './components/Hoc/Layout';
+import Layout from './Hoc/Layout';
 import Home from './components/Home';
-
+import RegisterLogin from './components/RegisterLogin';
 
 const Routes = () => {
   return(
     <Layout>
       <Switch>
+        <Route path="/register_login" exact component={RegisterLogin}/>
         <Route path="/" exact component={Home}/>
       </Switch>
     </Layout>
--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './assets/css/styles.css';
 
 import { BrowserRouter } from 'react-router-dom';
-import Routes from './routes';
+import Routes from './Routes';
 
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
