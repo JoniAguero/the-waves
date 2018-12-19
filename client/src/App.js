6cb06eaf2db8ec4e8bc0109ffcb79c614849fe5c
Joni Agüero <aguerojonathanivan@gmail.com>
2018-12-19 15:32:35 -0300

    assets

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 
 class App extends Component {
+
+  componentDidMount(){
+    axios.get('http://localhost:1234/api/product/articles').then(res => {
+      console.log(res);
+    })
+  }
+
   render() {
     return (
       <div className="App">
