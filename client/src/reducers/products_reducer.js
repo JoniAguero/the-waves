8e5e4cee38f47250c76ca82757e631f80eb9b721
Joni Agüero <aguerojonathanivan@gmail.com>
2018-12-20 18:25:31 -0300

    modified

--- client/src/reducers/products_reducer.js
+++ client/src/reducers/products_reducer.js
@@ -2,7 +2,12 @@ import {
     GET_PRODUCTS_BY_SELL,
     GET_PRODUCTS_BY_ARRIVAL,
     GET_BRANDS,
-    GET_WOODS
+    ADD_BRAND,
+    GET_WOODS,
+    ADD_WOOD,
+    GET_PRODUCTS_TO_SHOP,
+    ADD_PRODUCT,
+    CLEAR_PRODUCT
 } from '../actions/types';
  
 
@@ -14,8 +19,30 @@ export default function(state={},action){
             return {...state, byArrival:  action.payload }
         case GET_BRANDS:
             return {...state, brands: action.payload }
+        case ADD_BRAND:
+            return {
+                ...state, 
+                addBrand: action.payload.success , 
+                brands:action.payload.brands 
+            }
         case GET_WOODS:
             return {...state, woods: action.payload }
+        case ADD_WOOD:
+            return {
+                ...state, 
+                addWood: action.payload.success , 
+                woods:action.payload.woods 
+            }
+        case GET_PRODUCTS_TO_SHOP:
+            return {
+                ...state,
+                toShop: action.payload.articles,
+                toShopSize: action.payload.size
+            }
+        case ADD_PRODUCT:
+            return {...state,addProduct: action.payload}
+        case CLEAR_PRODUCT:
+            return {...state,addProduct: action.payload}
         default:
             return state;
     }
