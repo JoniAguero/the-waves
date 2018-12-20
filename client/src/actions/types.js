7968b91decfa937a129415f3203cb49e0531f8cf
Joni Agüero <aguerojonathanivan@gmail.com>
2018-12-20 15:40:21 -0300

    Products Action&Reducer

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,4 +1,9 @@
+/* ****** USER ****** */
 export const LOGIN_USER = 'login_user';
 export const REGISTER_USER = 'register_user';
 export const AUTH_USER = 'auth_user';
 export const LOGOUT_USER = 'logout_user';
+
+/* ****** PRODUCT ****** */
+export const GET_PRODUCTS_BY_SELL = 'get_products_by_sell';
+export const GET_PRODUCTS_BY_ARRIVAL = 'get_products_by_arrival';
